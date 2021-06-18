0323decc5f67e63b3c969b4bb4a2add10083eb39
Evgeniy Ivon <ivoneug@gmail.com>
2021-06-18 11:28:52 +0300

    Initial Commit

---
+++ TuneOn Radio/TuneOn_RadioApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuneOn_RadioApp.swift
+//  TuneOn Radio
+//
+//  Created by Evgeniy on 18.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct TuneOn_RadioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
