9728419a71a7a0e623bc529b1a3250349383880d
Evgeniy Ivon <ivoneug@gmail.com>
2023-04-20 16:49:37 +0100

    added dummy test target

---
+++ TuneOn RadioTests/TuneOn_RadioTests.swift
@@ -0,0 +1,25 @@
+//
+//  TuneOn_RadioTests.swift
+//  TuneOn RadioTests
+//
+//  Created by Evgeniy Ivon on 20.04.2023.
+//
+
+import XCTest
+@testable import TuneOn_Radio
+
+final class TuneOn_RadioTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        XCTAssertTrue(true)
+    }
+
+}
